67933f13a126ff643a9affadf68f9e6d06644db7
Dina Kendle <dina.kendle@gmail.com>
2020-04-27 15:44:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,11 +13,11 @@ class Triangle
       elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
         raise TriangleError
       else
-        (@side_1 == @side_2 && @side_2 == @side_3 && @side_1 == @side_3)
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
-        (@side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3)
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
         :isosceles
-        (@side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3)
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
         :scalene
       end
     end
