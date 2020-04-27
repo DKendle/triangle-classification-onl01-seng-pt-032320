0152035574677fe9e57581ad277a648f49b32ba9
Dina Kendle <dina.kendle@gmail.com>
2020-04-27 15:42:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,14 +8,16 @@ class Triangle
     end
     
     def kind 
-      if side_1 <= 0 || side_2 <= 0 || side_3 <=0 
+      if @side_1 <= 0 || @side_2 <= 0 || @side_3 <=0 
+        raise TriangleError
+      elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
         raise TriangleError
       else
-        (side_1 == side_2 && side_2 == side_3)
+        (@side_1 == @side_2 && @side_2 == @side_3)
         :equilateral
-        (side_1 == side_2 && side_2 != side_3)
+        (@side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3)
         :isosceles
-        (side_1 != side_2 && side_2 != side_3)
+        (@side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3)
         :scalene
       end
     end
